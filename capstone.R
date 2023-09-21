3cc126009d4b9976c9216b7f8d43ed921400554f
vachsark <142055112+vachsark@users.noreply.github.com>
2023-09-21 16:14:22 -0700

    Some changed and new graphs added.

--- capstone.R
+++ capstone.R
@@ -47,14 +47,13 @@ all_trips$day <- day(all_trips$date)
 all_trips$year <- year(all_trips$date)
 
 #removing lat,long
-all_trips <- all_trips %>% 
-  select(-c(start_lat,start_lng,end_lat,end_lng))
+#dont want to do this anymore | i want to see the distance that was traveled compared to the time it took to get there
+#all_trips <- all_trips %>% 
+ # select(-c(start_lat,start_lng,end_lat,end_lng))
 
-all_trips_safe <- all_trips
-head(all_trips_safe)
+
+
 # good up until here
-#just in case
-all_trips <- all_trips_safe
 
 glimpse(all_trips)
 
@@ -136,4 +135,65 @@ counts
 # the new file with all the data
 
 
-write.csv(counts, file = "C:\\Users\\vache\\OneDrive\\Desktop\\capstone\\avg_ride_length.csv")
+#write.csv(counts, file = "C:\\Users\\vache\\OneDrive\\Desktop\\capstone\\avg_ride_length.csv")
+#write.csv(all_trips_v2, file = "C:\\Users\\vache\\OneDrive\\Desktop\\capstone\\divvy_long_lat.csv")
+
+#long and lat changes
+glimpse(all_trips_v2)
+
+#lat diff in km
+#all_trips_v2$lat_diff <- abs(all_trips_v2$end_lat - all_trips_v2$start_lat) * 111.699
+
+#checking data
+#max(all_trips_v2$lat_diff, na.rm = TRUE)
+#min(all_trips_v2$lat_diff, na.rm = TRUE)
+#mean(all_trips_v2$lat_diff, na.rm = TRUE)
+
+#long Diff KM
+#all_trips_v2$long_diff <- abs(all_trips_v2$end_lng - all_trips_v2$start_lng) * 110.567
+
+#check
+#max(all_trips_v2$long_diff, na.rm = TRUE)
+#min(all_trips_v2$long_diff, na.rm = TRUE)
+#mean(all_trips_v2$long_diff, na.rm = TRUE)
+
+#in km
+all_trips_v2$totaldiff_miles <-((abs(all_trips_v2$end_lat - all_trips_v2$start_lat) * 69.4065408
+                           + abs(all_trips_v2$end_lng - all_trips_v2$start_lng) * 68.70314861))
+glimpse(all_trips_v2$totaldiff_miles)
+
+
+
+#cleaning up some extra columns
+all_trips_v2 <- all_trips_v2 %>% 
+  select(-c(latmean,lat_diff,long_diff))
+
+v2_average_miles <- all_trips_v2 %>%
+  mutate(weekday = wday(started_at, label = TRUE)) %>% 
+  group_by(member_casual, weekday) %>% 
+  summarise(number_of_rides = n(), average_miles = mean(totaldiff_miles, na.rm = TRUE)) %>% 
+  arrange(member_casual, weekday) %>% 
+  ggplot(aes(x = weekday, y = average_miles, fill= member_casual)) + geom_col(position = "dodge")
+v2_average_miles
+
+v1_electric_classic <- all_trips_v2 %>%
+  mutate(weekday = wday(started_at, label = TRUE)) %>% 
+  group_by(rideable_type, member_casual) %>% 
+  summarise(number_of_rides = n(), average_miles = mean(totaldiff_miles, na.rm = TRUE)) %>% 
+  arrange(rideable_type, member_casual) %>% 
+  ggplot(aes(x = member_casual, y = number_of_rides, fill= rideable_type)) + geom_col(position = "dodge")
+v1_electric_classic
+
+#write.csv(all_trips_v2, file = "C:\\Users\\vache\\OneDrive\\Desktop\\capstone\\divvy_everything.csv")
+## maybe check average distance traveled with each type of bike?
+
+
+## finding the most common start station and end station, some data is missing but it can still
+## give a good idea about where the casual group comes from and goes to most often.
+
+#for export
+all_trips_safe <- all_trips_v2 %>% 
+  select(-c(ride_length,start_lng,end_lat,end_lng, start_lat,start_station_id,end_station_id))
+head(all_trips_safe)
+
+write.csv(all_trips_safe, file = "C:\\Users\\vache\\OneDrive\\Desktop\\capstone\\divvy_cleaner.csv")
